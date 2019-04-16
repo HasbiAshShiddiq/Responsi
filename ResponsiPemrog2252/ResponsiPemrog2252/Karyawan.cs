5fbae131cc251ccbd70893d79e63ddf475216039
Hasbi Ash Shiddiq <abi.hasbiashshiddiq@gmail.com>
2019-04-16 14:16:42 +0700

    Responsi

---
+++ ResponsiPemrog2252/ResponsiPemrog2252/Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2252
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        
+        
+
+        
+
+        public Karyawan (string Nik, string Nama, int GajiBulanan)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+            this.GajiBulanan = GajiBulanan;
+        }
+        public void NaikGaji()
+        {
+            GajiBulanan = GajiBulanan + GajiBulanan / 10;
+        }
+    }
+}
---
+++ ResponsiPemrog2252/ResponsiPemrog2252/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2252
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Kar1 = new Karyawan("19030123", "Paijo", 300000);
+            Karyawan Kar2 = new Karyawan("19030124", "Jono", 200000);
+
+
+            Console.WriteLine("No Nik/Nama                  Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("1. {0} {1} {2}", Kar1.Nik, Kar1.Nama, Kar1.GajiBulanan);
+            Console.WriteLine("2. {0} {1} {2}", Kar2.Nik, Kar2.Nama, Kar2.GajiBulanan);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Kar1.NaikGaji();
+            Kar2.NaikGaji();
+
+            Console.WriteLine("No Nik/Nama                  Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("1. {0} {1} {2}", Kar1.Nik, Kar1.Nama, Kar1.GajiBulanan);
+            Console.WriteLine("2. {0} {1} {2}", Kar2.Nik, Kar2.Nama, Kar2.GajiBulanan);
+
+        }
+    }
+}
