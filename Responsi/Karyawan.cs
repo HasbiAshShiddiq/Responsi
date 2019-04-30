64c47021504bf905b095bdc08d30a40f8fc8a063
HasbiAshShiddiq <42289614+HasbiAshShiddiq@users.noreply.github.com>
2019-04-30 18:23:01 +0700

    Add files via upload

---
+++ Responsi/Karyawan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+
+        public Karyawan(string Nik, string Nama, int Gaji)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+            this.Gaji = Gaji;
+        }
+        public void Bonus()
+        {
+            Gaji = Gaji + Gaji / 10;
+        }
+
+    }
+}
---
+++ Responsi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Kar1 = new Karyawan("19030123", "Jono", 3000000);
+            Karyawan Kar2 = new Karyawan("19030123", "Paijo", 4000000);
+
+            if(Kar1.Gaji<=0)
+            {
+                Kar1.Gaji = 0;
+            }
+            else if(Kar1.Gaji>=0)
+            {
+                Kar1.Gaji = Kar1.Gaji;
+            }
+            Console.WriteLine("No. Nik/Nama                    Gaji");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. {0}/{1}         Rp.{2}", Kar1.Nik, Kar1.Nama, Kar1.Gaji);
+            Console.WriteLine("2. {0}/{1}        Rp.{2}", Kar2.Nik, Kar2.Nama, Kar2.Gaji);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Kar1.Bonus();
+            Kar2.Bonus();
+
+            Console.WriteLine("No. Nik/Nama                    Gaji");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. {0}/{1}         Rp.{2}", Kar1.Nik, Kar1.Nama, Kar1.Gaji);
+            Console.WriteLine("2. {0}/{1}        Rp.{2}", Kar2.Nik, Kar2.Nama, Kar2.Gaji);
+        }
+    }
+}
